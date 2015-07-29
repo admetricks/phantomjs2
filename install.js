6280fee7b2fc8bd8b2e7c16aa0a7bc54621fae04
Víctor Rojas <vitorohe@gmail.com>
2015-07-29 10:24:59 -0300

    Fix version in download url
    Add 'v'

--- install.js
+++ install.js
@@ -24,7 +24,7 @@ var util = require('util')
 var which = require('which')
 
 var cdnUrl = process.env.PHANTOMJS_CDNURL || 'https://github.com/bprodoehl/phantomjs/releases/download/'
-var downloadUrl = cdnUrl + helper.version + '/phantomjs-' + helper.version + '-'
+var downloadUrl = cdnUrl + 'v' + helper.version + '/phantomjs-' + helper.version + '-'
 
 var originalPath = process.env.PATH
 
